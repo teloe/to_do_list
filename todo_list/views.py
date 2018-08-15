9a86b7e63521b6106c44c971aac17d61d695bcef
Tom Eloe <tomeloe@gmail.com>
2018-08-14 20:33:39 -0700

    added delete method

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import List
 from .forms import ListForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 def home(request):
 
@@ -10,6 +12,7 @@ def home(request):
         if form.is_valid():
             form.save()
             all_items = List.objects.all
+            messages.success(request, ('Item added!'))
             return render(request, 'home.html', {'all_items': all_items})
     else:
         all_items = List.objects.all
@@ -18,3 +21,9 @@ def home(request):
 def about(request):
     context = {'first_name': 'Tom', 'last_name': 'Eloe'}
     return render(request, 'about.html', context)
+
+def delete(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request, ('Item removed.'))
+    return redirect('home')
