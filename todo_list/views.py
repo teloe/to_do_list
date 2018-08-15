20e6636e7f1cdd5682f69ad0b1c2b17a0574d6b9
Tom Eloe <tomeloe@gmail.com>
2018-08-14 20:42:30 -0700

    added cross off/ uncross

--- todo_list/views.py
+++ todo_list/views.py
@@ -27,3 +27,15 @@ def delete(request, list_id):
     item.delete()
     messages.success(request, ('Item removed.'))
     return redirect('home')
+
+def cross_off(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = True
+    item.save()
+    return redirect('home')
+
+def uncross(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = False
+    item.save()
+    return redirect('home')
